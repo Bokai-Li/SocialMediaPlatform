04223b8bec73d5882c74f56a0fd54c8f34c19460
Hanqi Zhang <hanqizhang@hanqis-mbp.home>
2019-12-12 03:20:59 -0500

    video version

--- ball/ball.js
+++ ball/ball.js
@@ -318,7 +318,7 @@ async function loadProfile(event){
   <article class="media">
     <div class="media-left">
       <figure class="image is-64x64">
-        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+        <img src="./cat.jpg" alt="Image">
       </figure>
     </div>
     <div class="media-content">
